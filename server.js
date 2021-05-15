8e8b2471af4789db72ba05e7ae90a1aa63a589f1
Tony <82540535+Bebe2158@users.noreply.github.com>
2021-05-14 20:58:02 -0700

    Add files via upload

--- server.js
+++ server.js
@@ -29,3 +29,83 @@ let app = http.createServer(
 ).listen(port);
 
 console.log('The server is running');
+
+
+/******************************/
+/* set up static file */
+
+const { Server } = require("socket.io");
+const io = new Server(app);
+
+io.on('connection', (socket) => {
+
+	/* output a log */
+	function serverLog(...messages){
+		io.emit('log',['**** Message from the server:\n']);
+		messages.forEach((item) => {
+			io.emit('log',['****\t'+item]);
+			console.log(item);
+		});
+    }
+
+    serverLog('a page connected to the server: '+socket.id);
+
+    socket.on('disconnect', () => {
+	    serverLog('a page disconnected from the server: '+socket.id);
+	});
+
+/* join room */
+    socket.on('join_room', (payload) => {
+        serverLog('Server received a command','\'join_room\'',JSON.stringify(payload));
+        if(( typeof payload == 'undefined') || (payload === null)){
+         response = {};
+         response.result = 'fail';
+         response.message = 'client did not send a payload';
+         socket.emit('join_room_response',response);
+         serverLog('join_room command failed', JSON.stringify(response));
+         return;
+        }
+        let room = payload.room;
+        let username = payload.username;
+        if(( typeof room == 'undefined') || (room === null)){
+         response = {};
+         response.result = 'fail';
+         response.message = 'client did not send a valid room to join';
+         socket.emit('join_room_response',response);
+         serverLog('join_room command failed', JSON.stringify(response));
+         return;
+        }
+        if(( typeof username == 'undefined') || (username === null)){
+         response = {};
+         response.result = 'fail';
+         response.message = 'client did not send a valid username to join ';
+         socket.emit('join_room_response',response);
+         serverLog('join_room command failed', JSON.stringify(response));
+         return;
+        }
+
+        socket.join(room);
+
+        io.in(room).fetchSockets().then((sockets)=>{
+            serverLog('There are'+sockets.length+' clients in the room, '+room);
+            if ((typeof sockets == 'undefined') || (sockets === null) || !sockets.includes(socket)){
+                response = {};
+                response.result = 'fail';
+                response.message = 'Server interal error';
+                socket.emit('join_room_response',response);
+                serverLog('join_room command failed', JSON.stringify(response));
+            }
+            else{
+                response = {};
+                response.result = 'succcess';
+                response.room = room;
+                response.username = username;
+                response.count = sockets.length;
+                io.of('/').to(room).emit('join_room_response',response);
+                serverLog('join_room command succeeded', JSON.stringify(response));
+            }
+        });
+	});
+}); 
+
+
